bef33b4e6c9cc4a438d796bdadeb4dabbda29243
Luna Fidalgo <lunafidalgo@gmail.com>
2018-04-24 10:51:04 +0200

    Previo Merge

--- routes/index.js
+++ routes/index.js
@@ -9,36 +9,5 @@ router.get("/", (req, res, next) => {
   res.render("index", { user: req.user });
 });
 
-router.get("/:id", (req, res, next) => {
-  //console.log(req.body.id_route);
-  console.log("eeee"+req.params.id)
-  let api_url =
-    "https://openbus.emtmadrid.es:9443/emt-proxy-server/last/bus/GetRouteLines.php";
-
-  const params = {
-    idClient: "WEB.SERV.lunafidalgo@gmail.com",
-    passKey: "7C6C9B0A-F2FA-4F74-8568-3D2858C136ED",
-    Lines: req.body.id_route,
-    SelectDate: "20/04/2018"
-  };
-
-  axios
-    .post(api_url, qs.stringify(params), {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    })
-    .then(a => {
-      console.log("??")
-      //console.log(a.data.resultValues);
-      res.render("error");
-    })
-    .catch(e => console.log(e));
-});
-
-// router.get("/lineStops", (req, res, next) => {
-// console.log("llega aqui")
-//   res.render("lineStops");
-// });
 
 module.exports = router;
