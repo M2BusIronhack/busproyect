15936e4102d8b15b927be1defb7ca68dd6377a2f
Luna Fidalgo <lunafidalgo@gmail.com>
2018-04-25 12:25:16 +0200

    Añadido ruta new comment

--- routes/comment.js
+++ routes/comment.js
@@ -5,16 +5,21 @@ const axios = require("axios");
 const qs = require("qs");
 const Comment = require("../models/Comment");
 
+
+commentRouter.get("/new/:id",(req, res, next) => {
+  console.log("aaaa")
+  })
+
 commentRouter.get("/:lineID", (req, res, next) => {
-  let line = req.params.lineID;
-  console.log(line);
+  let selectedLine = req.params.lineID;
+
   let api_url =
     "https://openbus.emtmadrid.es:9443/emt-proxy-server/last/bus/GetListLinesExtend.php";
 
   const params = {
     idClient: "WEB.SERV.lunafidalgo@gmail.com",
     passKey: "7C6C9B0A-F2FA-4F74-8568-3D2858C136ED",
-    Lines: line,
+    Lines: selectedLine,
     SelectDate: "20/04/2018"
   };
 
@@ -26,12 +31,18 @@ commentRouter.get("/:lineID", (req, res, next) => {
     })
     .then(a => {
 
-      Comment.findOne()
-      res.render("line", a.data.resultValues);
+      // Comment.find({line: selectedLine}).then( (a) => {
+      //   console.log(a)
+        
+      // })
 
-      res.render("error");
+      res.render("line", a.data.resultValues);
+   
+     // res.render("error");
     })
     .catch(e => console.log(e));
 });
+
+
 
 module.exports = commentRouter;
